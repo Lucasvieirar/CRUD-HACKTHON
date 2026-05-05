3ad7c7a9ee941aa6046734fc5064b3470d4565e6
Fernando Chaves <102880138+Fernando-CR19@users.noreply.github.com>
2026-05-05 14:59:30 +0000

    fix: arruma banco e cria front

--- produtos/views.py
+++ produtos/views.py
@@ -4,25 +4,25 @@ from .models import Produto
 
 class ProdutoListView(ListView):
     model = Produto
-    template_name = 'produto_list.html'
+    template_name = 'produtos/produto_list.html'
 
 class ProdutoDetailView(DetailView):
     model = Produto
-    template_name = 'produto_detail.html'
+    template_name = 'produtos/produto_detail.html'
 
 class ProdutoCreateView(CreateView):
     model = Produto
-    template_name = 'produto_form.html'
+    template_name = 'produtos/produto_form.html'
     fields = ['nome', 'preco', 'descricao']
     success_url = reverse_lazy('produto_list')
 
 class ProdutoUpdateView(UpdateView):
     model = Produto
-    template_name = 'produto_form.html'
+    template_name = 'produtos/produto_form.html'
     fields = ['nome', 'preco', 'descricao']
     success_url = reverse_lazy('produto_list')
 
 class ProdutoDeleteView(DeleteView):
     model = Produto
-    template_name = 'produto_confirm_delete.html'
+    template_name = 'produtos/produto_confirm_delete.html'
     success_url = reverse_lazy('produto_list')
